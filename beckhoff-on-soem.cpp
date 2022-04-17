aa4e2a6dab61f88ed99e4879108aaf2924fa33cd
amidg <amidg4x4dima@gmail.com>
2022-04-17 00:20:38 -0400

    working on this

--- beckhoff-on-soem.cpp
+++ beckhoff-on-soem.cpp
@@ -1,19 +1,31 @@
 // test sketch that shows how to use a simple I/O rack of 
 #include "stdio.h"
 #include "iostream"
+#include <string.h>
+#include "slavelist.h"
 #include "soem/ethercat.h"
 
+using namespace std;
+
 // slave definitions
-#define EK1100      1
-#define EL1008      2
-#define EL2008      3
-#define NUMBER_OF_SLAVES 3 // can be changed down the line
+int predefinedSlaveList[NUMBER_OF_SLAVES] = {EK1100, EL1008, EL2008};
 
-void initSlaves() { //initiate slaves
+// prog definitions
+char* ethName = "";
 
+// functions
+bool initSlaves(char* portName) { //initiate slaves
+    if ( !(ec_init(ethName) > 0) ) {
+        return false;
+    } else if (ec_init(ethName) > 0) {
+        printf("ec_init on %s succeeded.\n", ethName);
+
+        ec_configdc();
+
+    }   
 }
 
-void setOutput() { //set output at bit X
+void setOutput(uint32 slaveAddress, std::byte pin, bool value) { //set output at bit X
 
 }
 
@@ -21,8 +33,18 @@ void readInput() { // read input at bit X
 
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { // argv[0] is the 
+    // 1. initialize slave devices on the specified ethernet port
+    switch (argc) {
+    case 0:
+        return 0;
+    case 1:
+        ethName = argv[1];
+    }
 
+    if (!initSlaves(ethName)) { return 0; };
+
+    // 2. execute simple program below
 
 
     return 0;
---
+++ slavelist.h
@@ -0,0 +1,4 @@
+const int NUMBER_OF_SLAVES = 3; // can be changed down the line
+#define EK1100 1
+#define EL1008 2
+#define EL2008 3
