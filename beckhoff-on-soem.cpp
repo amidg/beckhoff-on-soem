cba9c16e307b4fa631dfd7b892bd43313644b9d7
amidg <amidg4x4dima@gmail.com>
2022-04-16 22:08:15 -0400

    started coding

---
+++ beckhoff-on-soem.cpp
@@ -0,0 +1,29 @@
+// test sketch that shows how to use a simple I/O rack of 
+#include "stdio.h"
+#include "iostream"
+#include "soem/ethercat.h"
+
+// slave definitions
+#define EK1100      1
+#define EL1008      2
+#define EL2008      3
+#define NUMBER_OF_SLAVES 3 // can be changed down the line
+
+void initSlaves() { //initiate slaves
+
+}
+
+void setOutput() { //set output at bit X
+
+}
+
+void readInput() { // read input at bit X
+
+}
+
+int main(int argc, char *argv[]) {
+
+
+
+    return 0;
+}
